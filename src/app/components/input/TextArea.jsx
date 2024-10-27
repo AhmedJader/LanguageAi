4e17de3d2dfcd0c0426154bed6ee2250fb2e91f6
Ahmed <Ahmedjader837@gmail.com>
2024-10-26 21:49:29 -0400

    same thing

--- src/app/components/input/TextArea.jsx
+++ src/app/components/input/TextArea.jsx
@@ -6,6 +6,7 @@ const TextArea = ({ id, value, onChange, placeholder }) => (
     id={id}
     className="flex-grow py-2.5 px-4 border-none focus:outline-none block w-full border-transparent 
     rounded-lg dark:bg-neutral-900 dark:border-transparent dark:text-neutral-400 resize-none"//removed rezise option from textarea component as it didnt look nice
+    //added flex-grow so the text area took all the space in div and pushs the icons down for the resize code to work properly.
     placeholder={placeholder}
     value={value}
     onChange={onChange}
