f35a457932e1c60dff20a1b21d167ac20cb511ee
Ahmed <Ahmedjader837@gmail.com>
2024-10-26 21:51:50 -0400

    incase i want to revert

--- src/app/components/input/TextArea.jsx
+++ src/app/components/input/TextArea.jsx
@@ -7,6 +7,7 @@ const TextArea = ({ id, value, onChange, placeholder }) => (
     className="flex-grow py-2.5 px-4 border-none focus:outline-none block w-full border-transparent 
     rounded-lg dark:bg-neutral-900 dark:border-transparent dark:text-neutral-400 resize-none"//removed rezise option from textarea component as it didnt look nice
     //added flex-grow so the text area took all the space in div and pushs the icons down for the resize code to work properly.
+    //if i want to make it resizable again, simply remove flex-grow from icons, resize-none from text-area, remove resize code from div and overflow-hidden
     placeholder={placeholder}
     value={value}
     onChange={onChange}
